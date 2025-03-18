ebef97ca9d4a83d77237aa8ef8e05ef4b49cd87f
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-18 00:22:20 +0000

    Add run param to authorize deployment route

--- web/src/routes/[orgName]/[projectName]/authorize/[run]/+page.svelte
+++ web/src/routes/[orgName]/[projectName]/authorize/[run]/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import Button from '$lib/components/button.svelte';
-	import Card from '$lib/components/card.svelte';
-	import Divider from '$lib/components/divider.svelte';
 	import Heading from '$lib/components/heading.svelte';
 	import Text from '$lib/components/text/text.svelte';
-	import Title from '$lib/components/title.svelte';
+	import TextLink from '$lib/components/text/textLink.svelte';
+	import { queries } from '$lib/queries';
+	import { createQuery } from '@tanstack/svelte-query';
 
-	// Get commit url
-	// Get pull request url
+	const run = createQuery(() =>
+		queries.runs.projectRuns(page.params.orgName, page.params.projectName)._ctx.get(page.params.run)
+	);
+
+	// SHow commit, show branch, show pr. Maybe do this with a details component
 </script>
 
 <!-- <Title title="Authorize Fork Deployment"></Title> -->
@@ -15,8 +19,9 @@
 <div class="mt-16 flex flex-col space-y-4">
 	<Heading size="lg" class="text-center">Authorize Fork Deployment</Heading>
 	<Text size="lg" class="text-center">
-		The Deployment for commit <span class="text-on-surface font-semibold">5c47dd2</span> on pull request
-		AlfieJones/alfiejones.dev#1 is awaiting authorization.
+		The Deployment for commit <TextLink class="text-on-surface font-semibold"
+			>{run.data.gitSha}</TextLink
+		> on pull request AlfieJones/alfiejones.dev#1 is awaiting authorization.
 	</Text>
 
 	<div class="flex flex-col justify-center">
